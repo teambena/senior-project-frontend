02d350433d10ed264e7079ec0ec6d719fe3a4991
teambenq <teetanic27@gmail.com>
2022-06-09 19:49:13 +0700

    Make a line chart.
    Make a line chart using customer and start date data.

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -16,24 +16,23 @@
                 </div>
             </div>
         </section>
-        <!-- Barchart -->
-        <!-- <section class="page-section q-mb-md" >
+        <section class="page-section q-mb-md" >
             <div class="container-fluid">
                 <div class="row q-col-gutter-x-md">
-                    <div class="col-md-6 comp-grid" >
+                    <div class="col-md-8 comp-grid" >
                         <q-card  :flat="isSubPage" class=" nice-shadow-18">
                             <div class="q-pa-md">
-                                <div class="text-weight-bold text-h5">Customer Today</div>
+                                <div class="text-weight-bold text-h5">New Chart 1</div>
                                 <div class="text-caption"></div>
                                 <q-separator class="q-my-md"></q-separator>
                                 <div class="row q-col-gutter-sm">
                                     <div class="col">
-                                        <api-data-source   api-path="components_data/barchart_customertoday"  :query-params="filters" v-slot="req">
+                                        <api-data-source   api-path="components_data/linechart_newchart1"  :query-params="filters" v-slot="req">
                                             <div style="position:relative;text-align:center">
                                                 <q-inner-loading :showing="req.loading">
                                                     <q-spinner-pie color="accent" size="5em" />
                                                 </q-inner-loading>
-                                                <vue-chart :chart-data="req.response" chart-type="bar" ></vue-chart>
+                                                <vue-chart :chart-data="req.response" chart-type="line" ></vue-chart>
                                             </div>
                                         </api-data-source>
                                     </div>
@@ -41,11 +40,9 @@
                             </div>
                         </q-card>
                     </div>
-                    <div class="col-md-6 comp-grid" >
-                    </div>
                 </div>
             </div>
-        </section> -->
+        </section>
     </q-page>
 </template>
 <script>
